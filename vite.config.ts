7d9f03279551ca64b38f7d1b8d5b262a9f907b28
cwc1222 <cwwalker088@gmail.com>
2024-04-30 22:50:24 -0400

    feat: Added sitemal and robots.txt

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
+import Sitemap from 'vite-plugin-sitemap';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import * as mdp from 'vite-plugin-markdown';
 
 const { plugin: mdPlugin, Mode } = mdp;
 
+import { readdirSync } from 'node:fs';
+
+const routes = [
+    "/articles",
+    "/projects",
+    "/about"
+]
+const articles = readdirSync("static/markdown/articles")
+    .map(f => f.split(".")[0])
+    .map(f => `/articles/${f}`);
+
 export default {
     plugins: [
         tsconfigPaths(),
         mdPlugin({ mode: [Mode.HTML, Mode.TOC] }),
+        Sitemap({dynamicRoutes: [...routes, ...articles]}),
     ],
     resolve: {
         alias: {
