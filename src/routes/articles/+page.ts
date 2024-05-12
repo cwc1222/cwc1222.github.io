a3be6639e011318d0cb7c08ddf9c90ad213fc6e0
cwc1222 <cwwalker088@gmail.com>
2024-05-12 16:59:43 -0400

    feat: Added basic article page

---
+++ src/routes/articles/+page.ts
@@ -0,0 +1,9 @@
+import type { PageLoad } from './$types';
+import { articleManager } from "$lib/core/article";
+
+export const load: PageLoad = async () => {
+  const articles = await articleManager.getArticles();
+  return {
+    articles
+  };
+}
--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -1,5 +1,10 @@
 
-<script lang="ts"></script>
+<script lang="ts">
+  import type { PageData } from './$types';
+  import "$lib/core/components/litjs/cwcshadowdom";
+
+  export let data: PageData;
+</script>
 
 <svelte:head>
   <title>Article</title> 
@@ -8,4 +13,6 @@
 <div>
   <h1>Article</h1>
   <hr>
+
+  <cwc-shadowdom htmlCtn={data.article.html}></cwc-shadowdom>
 </div>
